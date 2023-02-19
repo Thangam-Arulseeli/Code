f05b2c8b4c2915fa1472354073fda09dfb4eae09
Thangam Arulseeli <seeliseelan@gmail.com>
2023-02-20 04:58:23 +0530

    commit2

---
+++ Basics/js/first.js
@@ -0,0 +1,9 @@
+
+document.write("Welcome in JS printing");
+function myFunction() {
+      document.getElementById("demo2").innerHTML = "Paragraph changed.";
+      alert("Hai");
+      console.log("Checking");
+    }
+document.write("hai  hai  hai");
+document.getElementById("demo").innerHTML = "Hello JavaScript";
