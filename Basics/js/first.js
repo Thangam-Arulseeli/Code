bd5d4c2d71d13e4f8e42caeb529986b7dd5ee9cb
Thangam Arulseeli <seeliseelan@gmail.com>
2023-02-21 23:19:43 +0530

    Basic commits

--- Basics/js/first.js
+++ Basics/js/first.js
@@ -1,9 +1,11 @@
 
-document.write("Welcome in JS printing");
+
 function myFunction() {
-      document.getElementById("demo2").innerHTML = "Paragraph changed.";
-      alert("Hai External JS");
-      console.log("Function called in External JS File");
+     document.write("Welcome to JS display statement");
+     // document.getElementById("demo1").innerHTML = "Paragraph changed.";
+      //alert("Hai External JS");
+      console.log("Function call in External JS File");
     }
-document.write("hai  hai  hai");
-document.getElementById("demo").innerHTML = "Hello JavaScript";
+//document.write("hai  hai  hai");
+
+//document.getElementById("demo").innerHTML = "Hello JavaScript";
