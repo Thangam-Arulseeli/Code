ae1acc303ddecbbdba7b646cc30d11391c3f927a
Thangam Arulseeli <seeliseelan@gmail.com>
2023-04-26 14:48:11 +0530

    Save - 26.04.23

--- Basics/js/first.js
+++ Basics/js/first.js
@@ -1,11 +1,15 @@
 
-
+function getMessage(){
+  alert("Welcome");
+}
 function myFunction() {
      document.write("Welcome to JS display statement");
      // document.getElementById("demo1").innerHTML = "Paragraph changed.";
       //alert("Hai External JS");
       console.log("Function call in External JS File");
     }
+
+ 
 //document.write("hai  hai  hai");
 
 //document.getElementById("demo").innerHTML = "Hello JavaScript";
